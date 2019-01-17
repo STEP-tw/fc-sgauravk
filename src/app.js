928dbd2deab3703eef296a70375e9d19c436cdbc
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2019-01-17 10:29:52 +0530

    remove all nested if

--- src/app.js
+++ src/app.js
@@ -1,26 +1,19 @@
 const fs = require("fs");
 
+const getFiles = function(url) {
+  if (url == "/") {
+    return "./src/index.html";
+  }
+  return `.${url}`;
+};
+
 const app = (req, res) => {
-  let allFiles = [
-    "/src/images/freshorigins.jpg",
-    "/src/images/animated-flower-image-0021.gif",
-    "/src/main.css",
-    "/src/main.js"
-  ];
-  if (req.url == "/") {
-    fs.readFile("./src/index.html", "utf8", (err, data) => {
-      res.write(data);
-      res.statusCode = 200;
-      res.end();
-    });
-  }
-  if (allFiles.includes(req.url)) {
-    fs.readFile(`.${req.url}`, (err, data) => {
-      res.write(data);
-      res.statusCode = 200;
-      res.end();
-    });
-  }
+  let file = getFiles(req.url);
+  fs.readFile(file, (err, data) => {
+    res.write(data);
+    res.statusCode = 200;
+    res.end();
+  });
 };
 
 module.exports = app;
