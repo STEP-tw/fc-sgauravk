33e82cb8b30e74000e70546705db2f8adf47f9d5
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2019-01-18 11:01:49 +0530

    extrect a feature to show the users comments

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const comments = require("./comments.json");
+const { createTable, arrangeCommentDetails } = require("./main.js");
 
 const getFiles = function(url) {
   if (url == "/") {
@@ -7,16 +9,44 @@ const getFiles = function(url) {
   return `.${url}`;
 };
 
+const getHtml = function(res) {
+  fs.readFile("./src/guestBook.html", (err, data) => {
+    data += createTable(comments);
+    res.write(data);
+    res.end();
+  });
+  return;
+};
+
+const extrectUserComments = function(req) {
+  let content = "";
+  req.on("data", chunk => {
+    content += chunk;
+  });
+  req.on("end", () => {
+    console.log(content);
+    comments.unshift(arrangeCommentDetails(content));
+    fs.writeFile("./src/comments.json", JSON.stringify(comments), err => {
+      return;
+    });
+  });
+};
+
 const app = (req, res) => {
-  let file = getFiles(req.url);
-  fs.readFile(file, (err, data) => {
+  if (req.url == "/src/guestBook.html" && req.method == "POST") {
+    extrectUserComments(req);
+    getHtml(res);
+    return;
+  }
+  let filePath = getFiles(req.url);
+  fs.readFile(filePath, (err, content) => {
     try {
-      res.write(data);
       res.statusCode = 200;
+      res.write(content);
       res.end();
     } catch (err) {
-      res.write("Not Found");
       res.statusCode = 404;
+      res.write("Not Found");
       res.end();
     }
   });
--- src/main.js
+++ src/main.js
@@ -5,3 +5,33 @@ const hideGif = function() {
     waterJar.style.visibility = "visible";
   }, 1000);
 };
+
+const withTag = function(content, tag){
+  return `<${tag}>${content}</${tag}>`
+}
+
+const createTableRow = function(object){
+  let row = "";
+  row = row + withTag(object.time, "td");
+  row = row + withTag(object.name, "td");
+  row = row + withTag(object.comment, "td");
+  return withTag(row, "tr");
+};
+
+const createTable = function(list){
+  let table = [];
+  let tableHeading = {time: "DATE_TIME", name: "NAME", comment: "COMMENT"};
+  let firstLine = withTag(createTableRow(tableHeading), "th");
+  table.push(firstLine);
+  list.map(element => table.push(createTableRow(element)));
+  return table.join("");
+}
+
+const arrangeCommentDetails = function(details) {
+  let time = new Date().toLocaleString();
+  let name = details.split("&")[0].split("=")[1];
+  let comment = details.split("&")[1].split("=")[1];
+  return { name, comment, time };
+ };
+
+ module.exports = {createTable, arrangeCommentDetails};
